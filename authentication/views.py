7f75db7e03a7b7f0cd58ec57e28257f9c589b0a5
Remi288 <iamopeoluwaadewale_28@yahoo.com>
2021-02-04 02:20:50 +0100

    register/login endpoint

---
+++ authentication/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render
+from rest_framework.generics import GenericAPIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.conf import settings
+from django.contrib import auth
+import jwt
+
+from .serializers import UserSerializers
+
+
+# Create your views here.
+
+class RegisterView(GenericAPIView):
+    serializer_class = UserSerializers
+
+    def post(self, request):
+        serializer = UserSerializers(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+
+            status_code = status.HTTP_201_CREATED
+            response = {
+                'message': 'successfully!',
+                'payload': serializer.data
+            }
+            return Response(response, status=status_code)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginView(GenericAPIView):
+
+    def post(self, request):
+        data = request.data
+        username = data.get('username', '')
+        password = data.get('password', '')
+        user = auth.authenticate(username=username, password=password)
+
+        if user:
+            auth_token = jwt.encode({'username': user.username}, settings.JWT_SECRET_KEY)
+
+            serializer = UserSerializers(user)
+
+            data = {
+                'payload': serializer.data,
+                "token": auth_token
+            }
+            return Response(data, status=status.HTTP_200_OK)
+
+        return Response({"message": 'invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
